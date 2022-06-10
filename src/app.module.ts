b7a6939be9fc85e3f433435021b2325cae294320
gentstats <danielplan91@gmail.com>
2022-06-10 15:21:07 +0200

    menus resource scaffold

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongoModule } from './mongo/mongo.module';
+import { MenusModule } from './menus/menus.module';
 
 @Module({
-  imports: [MongoModule],
+  imports: [MongoModule, MenusModule],
   controllers: [AppController],
   providers: [AppService],
 })
