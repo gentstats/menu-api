a2cefb351e1b9702497ec57dcb6c3cc88d3bdc75
gentstats <danielplan91@gmail.com>
2022-06-10 20:44:30 +0200

    add new service dependencies to controller testing module

--- src/menus/menus.controller.spec.ts
+++ src/menus/menus.controller.spec.ts
@@ -1,21 +1,39 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { connections } from 'mongoose';
+import { MongoModule } from '../mongo/mongo.module';
 import { MenuEntity } from './entities/menu.entity';
 import { MenusController } from './menus.controller';
 import { MenusService } from './menus.service';
+import { MenuRepository } from './repositories/menu.repository';
+import { Menu, MenuSchema } from './schemas/menu.schema';
 import { menuStub } from './stubs/menu.stub';
 
 describe('MenusController', () => {
   let controller: MenusController;
   let service: MenusService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        MongoModule,
+        MongooseModule.forFeature([
+          {
+            name: Menu.name,
+            schema: MenuSchema,
+          },
+        ]),
+      ],
       controllers: [MenusController],
-      providers: [MenusService],
+      providers: [MenusService, MenuRepository],
     }).compile();
 
     controller = module.get<MenusController>(MenusController);
     service = module.get<MenusService>(MenusService);
+  });
+
+  afterAll(async () => {
+    await connections[1].close();
   });
 
   it('should be defined', () => {
