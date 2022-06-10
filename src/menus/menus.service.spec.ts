e729dfa2a58d1e303bfefbc27f3630baa53108b0
gentstats <danielplan91@gmail.com>
2022-06-10 16:25:19 +0200

    service specification

--- src/menus/menus.service.spec.ts
+++ src/menus/menus.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Menu } from './entities/menu.entity';
 import { MenusService } from './menus.service';
+import { menuStub } from './stubs/menu.stub';
 
 describe('MenusService', () => {
   let service: MenusService;
@@ -14,5 +16,20 @@ describe('MenusService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getMenu', () => {
+    it('should return the menu', () => {
+      expect(service.getMenu()).toMatchObject(menuStub());
+    });
+
+    it('should return instance of Menu', () => {
+      expect(service.getMenu()).toBeInstanceOf(Menu);
+    });
+
+    it('should contain a title', () => {
+      const menu: Menu = service.getMenu();
+      expect(menu).toMatchObject({ title: 'I am a title' });
+    });
   });
 });
