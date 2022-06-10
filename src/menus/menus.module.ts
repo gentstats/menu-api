ea846061a7fcd793c4ffb804091cdf0ab936075a
gentstats <danielplan91@gmail.com>
2022-06-10 19:24:45 +0200

    define testing module

--- src/menus/menus.module.ts
+++ src/menus/menus.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { MenusService } from './menus.service';
 import { MenusController } from './menus.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Menu, MenuSchema } from './schemas/menu.schema';
+import { MongoModule } from '../mongo/mongo.module';
 
 @Module({
+  imports: [
+    MongoModule,
+    MongooseModule.forFeature([
+      {
+        name: Menu.name,
+        schema: MenuSchema,
+      },
+    ]),
+  ],
   controllers: [MenusController],
-  providers: [MenusService]
+  providers: [MenusService],
 })
 export class MenusModule {}
--- src/menus/repositories/menu.repository.spec.ts
+++ src/menus/repositories/menu.repository.spec.ts
@@ -1,17 +1,35 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { connections } from 'mongoose';
+import { MongoModule } from '../../mongo/mongo.module';
 import { MenusModule } from '../menus.module';
+import { Menu, MenuSchema } from '../schemas/menu.schema';
 import { MenuRepository } from './menu.repository';
 
 describe('MenusController', () => {
   let repository: MenuRepository;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [MenusModule],
+      imports: [
+        MongoModule,
+        MongooseModule.forFeature([
+          {
+            name: Menu.name,
+            schema: MenuSchema,
+          },
+        ]),
+      ],
       providers: [MenuRepository],
     }).compile();
 
     repository = module.get<MenuRepository>(MenuRepository);
+  });
+
+  afterAll(() => {
+    connections.map(async (conn) => {
+      await conn.close();
+    });
   });
 
   it('should be defined', () => {
